dd34091dcd139cdb1ea7bef2e1ca3d8a60bdbcb7
zahidkhanjs <zahidshahwani9925@gmail.com>
2025-11-17 21:55:13 +0500

    project initlize

---
+++ varible/app.js
@@ -0,0 +1,18 @@
+// let username = 'sabir khan'
+// console.log(username)
+
+
+
+
+// let x = 'sabir khan';
+// let age = 43;
+// console.log(x , age );
+
+// let happy = true;
+// let disappy = false;
+// console.log(happy , disappy);
+
+
+
+// let username = "zahidkhan"
+// let userIntro = 'i am a ' + username
