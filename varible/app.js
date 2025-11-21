cd7032194e5793573b6600e05a86d47ed901b322
zahidkhanjs <zahidshahwani9925@gmail.com>
2025-11-21 07:21:37 +0500

    d

--- varible/app.js
+++ varible/app.js
@@ -1,9 +1,6 @@
 // let username = 'sabir khan'
 // console.log(username)
 
-
-
-
 // let x = 'sabir khan';
 // let age = 43;
 // console.log(x , age );
@@ -12,7 +9,27 @@
 // let disappy = false;
 // console.log(happy , disappy);
 
-
-
 // let username = "zahidkhan"
 // let userIntro = 'i am a ' + username
+
+// let username = 'zahidkhan';
+// let age = 34;
+// let userIntro = 'i am a ' + username + age;
+
+// let firstname = zahid;
+// let lastname = khan;
+// let userIntro = 'i am a ' + firstname + lastname;
+
+// let firstname = "zahid";
+// let lastname = "khan";
+// let userIntro = "i am a " + firstname + lastname;
+
+// let firstname = " zahi/d";
+// let lastname = "khan";
+// let age = 20;
+// let userIntro = "i am " + firstname + lastname + ". my age is " + age;
+
+
+
+
+
