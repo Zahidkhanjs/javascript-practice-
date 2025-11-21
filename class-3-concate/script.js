fd1c40e91757bfb73423d9f3780421d47a2e2346
zahidkhanjs <zahidshahwani9925@gmail.com>
2025-11-21 19:36:12 +0500

    sa

---
+++ class-3-concate/script.js
@@ -0,0 +1,30 @@
+// let streat = 'balochistan universty'
+// let country = 'pakistan '
+// let fulinformation = streat + " " + country
+// console.log (fulinformation)
+
+// let information = "Your new name is ";
+// let username = 'zahidkhan';
+// let number = '9925';
+// let detailes = information + username + number;
+// console.log (detailes);
+
+// let firstname = "zahid";
+// let lastname = "khan";
+// let email = "@gmail.com";
+// let full = firstname + lastname + email;
+// console.log(full);
+
+
+// let favorit = 'your favorit thing are'
+// let food = 'biryani'
+// let animal =  'Hourse'
+// let color = 'green'
+// let some = favorit + ' ' + food + ' ' + animal + ' ' + color 
+// console.log(some)
+
+
+// let pgs = 'the only limit is your maind '
+// let username = '- john Don'
+// // let full = pgs + " " + username 
+// console.log(full)
