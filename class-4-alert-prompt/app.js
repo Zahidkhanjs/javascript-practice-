aa1d19e07f513e4d604bb5f701c9298fe218c38f
zahidkhanjs <zahidshahwani9925@gmail.com>
2025-11-21 20:36:34 +0500

    sdf

---
+++ class-4-alert-prompt/app.js
@@ -0,0 +1,37 @@
+// var  username = prompt('what is user name')
+// var welcome = alert ('Welcome ')
+// console.log(username + welcome)
+
+// var username = prompt ("what is your name ")
+// var age = prompt ( 'how old age are your')
+// console.log(username + age )
+
+// let color = prompt ('your favorit color ')
+// let wow = alert (' "wow" color is  a nice color ')
+// console.log(color + wow)
+
+// let num1 = number(prompt("Enter first Number"));
+// let num2 = number(pormpt("Enter last Number"));
+// let sum = sum + sum;
+// console.log(sum);
+
+
+//  var usernaem = prompt ('username')
+// var hoby = pormpt ('what is your hoby')
+//  var country = prompt ("Name of your country")
+//  let ful = alert (username + ' ' + hoby + ' ' + country)
+
+
+
+
+// var username = prompt("Username");
+// var hobby = prompt("What is your hobby?");
+// var country = prompt("Name of your country");
+
+// alert(username + " " + hobby + " " + country);
+
+
+// var usernaem = prompt('what is your name ')
+// var hobby = prompt ('what is your hobby')
+// var country = prompt ('country name')
+// alert(usernaem + " " + hobby + " " +country)
